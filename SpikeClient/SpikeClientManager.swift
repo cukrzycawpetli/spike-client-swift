b37206bc1d2c62889c2aa1d4245bc1e625820f0f
cukrzycawpetli <49746355+cukrzycawpetli@users.noreply.github.com>
2019-04-18 09:40:52 +0200

    Update SpikeClientManager.swift
    Change name from Spike to NS ShareServer

--- SpikeClient/SpikeClientManager.swift
+++ SpikeClient/SpikeClientManager.swift
@@ -32,7 +32,7 @@ public class SpikeClientManager: CGMManager {
         }
     }
 
-    public static let localizedTitle = LocalizedString("Spike", comment: "Title for the CGMManager option")
+    public static let localizedTitle = LocalizedString("NS ShareServer", comment: "Title for the CGMManager option")
 
     public let appURL: URL? = URL(string: "spikeapp://")
 
